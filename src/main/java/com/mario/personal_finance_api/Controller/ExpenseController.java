436942ceb0ca8d1822220c0b9b01b99a31b8fa01
Mario Granados <94648274+MarioGranados@users.noreply.github.com>
2026-05-29 01:05:38 +0000

    Pending changes exported from your codespace

--- src/main/java/com/mario/personal_finance_api/Controller/ExpenseController.java
+++ src/main/java/com/mario/personal_finance_api/Controller/ExpenseController.java
@@ -12,11 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mario.personal_finance_api.Models.Expense;
-import com.mario.personal_finance_api.Models.User;
-import com.mario.personal_finance_api.Repository.ExpenseRepo;
-import com.mario.personal_finance_api.Repository.UserRepo;
-
+import com.mario.personal_finance_api.Models.*;
+import com.mario.personal_finance_api.Repository.*;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,6 +24,7 @@ public class ExpenseController {
 
     private final ExpenseRepo expenseRepo;
     private final UserRepo userRepo;
+    private final CategoryRepo categoryRepo;
 
     @GetMapping("/user/{user_id}")
     public ResponseEntity<List<Expense>> getUserExpensesById(@PathVariable Long user_id) {
@@ -37,8 +35,19 @@ public class ExpenseController {
     @PostMapping("/user/{user_id}")
     public ResponseEntity<Expense> createExpense(@RequestBody Expense expense, @PathVariable @NonNull Long user_id) {
         User user = userRepo.findById(user_id).orElseThrow(() -> new RuntimeException("Error fetching user"));
+        Category category = categoryRepo.findByCategoryName(expense.getCategory());
+
+        if(category == null) {
+            category = new Category();
+            category.setCategoryName();
+            category.setUser(user);
+
+            categoryRepo.save(category);
+        }
         expense.setUser(user);
+        expense.setCategory(category);
         return ResponseEntity.ok(expenseRepo.save(expense));
     }
+k
 
 }
